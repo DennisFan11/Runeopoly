a3ae93c14a0a94a9e0188e929d1decaa3434128a
DennisFan <dennis515jd@gmail.com>
2024-11-27 18:06:16 +0800

    ani save 11/27

--- GameLoopFSM/UI/ui.gd
+++ GameLoopFSM/UI/ui.gd
@@ -8,17 +8,37 @@ signal EXIT
 
 func ENTER():
 	$AnimationPlayer.play("Enter")
+	%RollButton.text = "Roll"
+	%RollButton.disabled = false
 	visible = true
-	_roll_dice()
-	%RollNumber.text = "Roll: " + str(_rolled_number)
+	
+var _rolled_number = 0
+func _on_roll_button_pressed() -> void:
+	%RollButton.disabled = true
+	const _count:int = 30
+	const _time:float = 0.03
+	for i in range(_count):
+		get_tree().create_timer(i * _time).timeout.connect(_random_roll)
+	get_tree().create_timer(_count * _time).timeout.connect(_roll_dice)
+	
+
+func _random_roll():
+	%RollButton.text = "Roll:" + str(randi_range(1, 12))
+	
+func _roll_dice():
+	_rolled_number = randi_range(1, 12) # 設置隨機數
+	if _is_select():
+		_rolled_number = _selectNum
+	%RollButton.text = "Roll:" + str(_rolled_number)
+	get_tree().create_timer(ROLL_TIME).timeout.connect(_EXIT) # TEST
+	
+
 func _EXIT():
 	EXIT.emit(_rolled_number)
 
 
-var _rolled_number = 0
-func _roll_dice():
-	_rolled_number = randi_range(1, 12) # 設置隨機數
-	get_tree().create_timer(ROLL_TIME).timeout.connect(_EXIT) # TEST
+
+
 
 
 
@@ -55,5 +75,27 @@ signal CUT_SCENE_EXIT
 
 
 
+var _selectNum:int = -1
+var _enable_select:bool
+
+func _is_select()-> bool:
+	return _enable_select and _selectNum != -1
+
 
 #
+func _selcetNum()-> int:
+	if _enable_select:
+		var text:String = %NumberInput.text
+		if text.is_valid_int():
+			return int(text)
+	return -1
+		
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	_enable_select = toggled_on
+	%NumberInput.visible = toggled_on
+	_selectNum = _selcetNum()
+		
+	
+func _on_number_input_text_changed(new_text: String) -> void:
+	_selectNum = _selcetNum()
